bc43e893503f88c3d0d1306aa34a0f794d0b8b6a
Fac2Rise <junelim1212@gmail.com>
2025-10-25 21:52:00 +0800

    Initialized the models and lighting for the model of MacBook

---
+++ macBook-GSAP-app/src/components/StudioLights.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Environment } from '@react-three/drei'
+import { Lightformer } from '@react-three/drei'
+
+const StudioLights = () => {
+  return (
+    <group name="lights">
+        <Environment resolution={256}>
+           <group>
+            <Lightformer 
+                form='rect'
+                intensity={10}
+                position={[-10, 5, -5]}
+                scale={10}
+                rotation-y={Math.PI / 2}
+            />
+            <Lightformer 
+                form='rect'
+                intensity={10}
+                position={[10, 0, 1]}
+                scale={10}
+                rotation-y={Math.PI / 2}
+            />
+           </group>
+        </Environment>
+    </group>
+  )
+}
+
+export default StudioLights
